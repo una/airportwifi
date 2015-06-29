4f3f13e2679a1db69c9659111a543f7b5dec7d06
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-06-28 23:07:24 -0500

    redis server set up

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
+// app stuff :)
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
+// url encoding for submission data
+var bodyParser = require('body-parser');
+var urlEncode = bodyParser.urlencoded({extended: false});
+
+app.use(express.static(__dirname + '/public'));
+
+// Redis connection
+var redis = require('redis');
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  var client = redis.createClient();
+  client.select((process.env.NODE_ENV || 'development').length);
+}
+
+// flushes db tests
+client.select((process.env.NODE_ENV || 'development').length);
 
 app.get('/airports', function(req,res) {
-  var airports = ['Austin', 'Nashville'];
-  res.json(airports);
+  client.hkeys('airports', function(err, names) {
+    if(err) throw err;
+    res.json(names);
+  });
 });
 
-app.post('/airports', function(req, res){
-  res.sendStatus(201);
+app.post('/airports', urlEncode, function(req, res){
+    var newAirport = req.body;
+    client.hset('airports', newAirport.name, newAirport.description, function(err) {
+        if(err) throw err;
+        res.status(201).json(newAirport.name);
+    });
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 var request = require('supertest');
 var app = require('./app');
 
+//test db
+var redis = require('redis');
+var client = redis.createClient();
+client.select('test'.length);
+client.flushdb();
+
 describe('Requests to the root path', function () {
     it('Returns a 200 status code', function(done) {
       request(app)
@@ -37,12 +43,12 @@ describe('Listing Airports', function () {
   it('Returns initial airports', function(done) {
     request(app)
       .get('/airports')
-      .expect(JSON.stringify(['Austin', 'Nashville']), done);
+      .expect(JSON.stringify([]), done);
   });
 });
 
 describe('Adding new airports', function () {
-  it('Resturns a 201 status code', function (done) {
+  it('Returns a 201 status code', function (done) {
       request(app)
         .post('/airports')
         .send('name=Name&description=this+is+the+description')
@@ -53,6 +59,6 @@ describe('Adding new airports', function () {
       request(app)
         .post('/airports')
         .send('name=Name&description=this+is+the+description')
-        .expect(/Name/i, done);
+        .expect(/name/i, done);
   });
 });
